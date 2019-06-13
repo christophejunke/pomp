8a07a4c0a0bb46e8eff739061e57686825431a06
Christophe Junke <junke.christophe@gmail.com>
2019-06-13 12:13:33 +0200

    Better error reporting

--- format.lisp
+++ format.lisp
@@ -1,31 +1,45 @@
 (in-package :pomp)
 
-(defun tokenize (pomp-format-string)
-  (let ((tokens)
-        (size 32)
+(defun map-tokens (function pomp-format-string)
+  (let ((size 32)
+        (counter 0)
         (modifier 0)
         (factor 1)
-        (state nil))
+        (state nil)
+        (start 0)
+        (position 0))
     (labels ((prefix (ratio next-state)
                (setf factor ratio)
+               (setf counter 0)
                (augment-prefix)
                (setf state next-state))
 
-             (expect (expected &key then)
-               (setf state (lambda (c)
-                             (assert (char= c expected))
-                             (etypecase then
-                               (keyword (token then))
-                               (function (setf state then))))))
+             (expect (expected &key then because)
+               (setf state
+                     (lambda (char)
+                       (assert (char= char expected)
+                               ()
+                               "Unexpected char ~s in place of ~s~
+                               ~@[ ~a~]."
+                               char
+                               expected
+                               (case because
+                                 (:buffer "in buffer format (%p%u)")))
+                       (etypecase then
+                         (keyword (token then))
+                         (function (setf state then))))))
 
              (augment-prefix ()
+               (incf counter)
                (setf modifier (clamp (* modifier factor) 1/4 2)))
 
-             (token (&rest args)
-               (push args tokens)
+             (token (&rest token)
+               (funcall function token)
+               (setf start position)
                (setf state #'state/%))
 
-             (counter-size () (* size modifier))
+             (size-for-prefix ()
+               (* size modifier))
 
              (state/dispatch (c)
                (setf modifier 1)
@@ -40,7 +54,9 @@
                  (#\p (setf state
                             (expect #\%
                                     :then (expect #\u
-                                                  :then :buffer))))
+                                                  :then :buffer
+                                                  :because :buffer)
+                                    :because :buffer)))
                  (#\x (token :file-descriptor))))
 
              (state/string (c)
@@ -50,18 +66,21 @@
              (state/half (c)
                (ecase c
                  (#\h (augment-prefix))
-                 ((#\i #\d) (token :signed (counter-size)))
-                 (#\u (token :unsigned (counter-size)))))
+                 ((#\i #\d) (token :signed (size-for-prefix)))
+                 (#\u (token :unsigned (size-for-prefix)))))
 
              (state/long (c)
                (ecase c
-                 (#\l (augment-prefix))
-                 ((#\i #\d) (token :signed (counter-size)))
-                 (#\u (token :unsigned (counter-size)))
+                 (#\l (augment-prefix)
+                  (assert (<= counter 2) () "Too many prefix characters."))
+                 ((#\i #\d) (token :signed (size-for-prefix)))
+                 (#\u (token :unsigned (size-for-prefix)))
                  ((#\f #\F #\g #\G #\e #\E)
-                  (ecase modifier
-                    (1 (token :single))
-                    (2 (token :double))))))
+                  (assert (= counter 1)
+                          ()
+                          "Unexpected float format with more than one prefix %l.")
+                  (ecase counter
+                    (1 (token :double))))))
 
              (state/% (c)
                (ecase c
@@ -69,8 +88,22 @@
                   (setf state #'state/dispatch)))))
       (loop
         initially (setf state #'state/%)
-        for c across pomp-format-string
-        do (funcall state c)
+        for char across pomp-format-string
+        do (incf position)
+           (handler-case (funcall state char)
+             (error (condition)
+               (error
+                "~&Invalid character ~s in ~<~s at position ~d.~@:_~
+                ~:*~v@T ^~:>~%~a"
+                char
+                (list pomp-format-string
+                      position)
+                condition)))
         finally
-           (assert (eq state #'state/%) () "Not enough input")
-           (return (nreverse tokens))))))
+           (assert (eq state #'state/%)
+                   ()
+                   "Not enough input for sequence ~s started at ~
+                    position ~d of ~s."
+                   (subseq pomp-format-string start)
+                   start
+                   pomp-format-string)))))
