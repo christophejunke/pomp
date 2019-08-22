8b6501290e69e93f517dc919487d169ad442a41b
Christophe Junke <junke.christophe@gmail.com>
2019-08-22 22:36:19 +0200

    whitespace-cleanup

--- test.lisp
+++ test.lisp
@@ -1,8 +1,8 @@
 (in-package :pomp)
 
 (assert (= 3 (let ((c 0))
-               (do-tokens (nil "%d%u%hhd" c)
-                 (incf c)))))
+	       (do-tokens (nil "%d%u%hhd" c)
+		 (incf c)))))
 
 (do-tokens (tok "%d%u%hhd")
   (assert (consp tok)))
@@ -24,50 +24,50 @@
    (dotimes (i 10000)
      (let ((v (random #.(1- (expt 2 64)))))
        (with-input-from-sequence (in
-                                  (with-output-to-sequence (out)
-                                    (%varint64e v out)))
-         (assert (= v (%varint64d in)))))))
+				  (with-output-to-sequence (out)
+				    (%varint64e v out)))
+	 (assert (= v (%varint64d in)))))))
   (time
    (dotimes (i 10000)
      (let ((v (random #.(1- (expt 2 32)))))
        (with-input-from-sequence (in
-                                  (with-output-to-sequence (out)
-                                    (%varint32e v out)))
-         (assert (= v (%varint32d in))))))))
+				  (with-output-to-sequence (out)
+				    (%varint32e v out)))
+	 (assert (= v (%varint32d in))))))))
 
 (block there-and-back-again
   (let* ((id (random #xFFFFFFFF))
-         (arguments (list
-                     (argument :signed-byte 127)
-                     (argument :unsigned-byte 255)
-                     (argument :signed-short -10201)
-                     (argument :unsigned-short 4000)
-                     (argument :signed-zig-varint -75000)
-                     (argument :unsigned-varint 32010)
-                     (argument :signed-zig-varlong -50505050505050)
-                     (argument :unsigned-varlong 10000000000999484838)
-                     (argument :ascii (ascii "AZERTYUIOP"))
-                     (argument :buffer (buffer #(10 0 30 0 50 10 20)))
-                     (argument :single-float 1e9)
-                     (argument :double-float pi)
-                     (argument :file-descriptor 53)))
-         (message (make-message id arguments)))
+	 (arguments (list
+		     (argument :signed-byte 127)
+		     (argument :unsigned-byte 255)
+		     (argument :signed-short -10201)
+		     (argument :unsigned-short 4000)
+		     (argument :signed-zig-varint -75000)
+		     (argument :unsigned-varint 32010)
+		     (argument :signed-zig-varlong -50505050505050)
+		     (argument :unsigned-varlong 10000000000999484838)
+		     (argument :ascii (ascii "AZERTYUIOP"))
+		     (argument :buffer (buffer #(10 0 30 0 50 10 20)))
+		     (argument :single-float 1e9)
+		     (argument :double-float pi)
+		     (argument :file-descriptor 53)))
+	 (message (make-message id arguments)))
     (multiple-value-bind (args id%) (pomp-decode message)
       (assert (equalp id% id))
       (assert (equalp args arguments))
-      (assert 
-       (equalp 
+      (assert
+       (equalp
 	(with-output-to-sequence (stream)
-          (write-binary (make-message id args) stream))
-        (concatenate 'simple-vector
-                     #(80 79 77 80)
-                     (with-output-to-sequence (o)
-                       (write-binary-type id '(unsigned-byte 32) o))
-                     #(90 0 0 0 1 127 2 255 3 39 216 4 160 15 5
-                       239 147 9 6 138 250 1 7 179 253 193 249 227 
-		       251 22 8 166 219 235 171 204 224 200 227 138 
+	  (write-binary (make-message id args) stream))
+	(concatenate 'simple-vector
+		     #(80 79 77 80)
+		     (with-output-to-sequence (o)
+		       (write-binary-type id '(unsigned-byte 32) o))
+		     #(90 0 0 0 1 127 2 255 3 39 216 4 160 15 5
+		       239 147 9 6 138 250 1 7 179 253 193 249 227
+		       251 22 8 166 219 235 171 204 224 200 227 138
 		       1 9 11 65 90 69 82 84 89 85 73 79
-                       80 0 10 7 10 0 30 0 50 10 20 11 40 107 110 78 
+		       80 0 10 7 10 0 30 0 50 10 20 11 40 107 110 78
 		       12 24 45 68 84 251 33 9 64 13 53 0 0 0)))))))
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
@@ -92,4 +92,3 @@
 	      ((pong id text)
 	       :pong)
 	      (t :no))))
-
